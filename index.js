ba361db9a985400c753d79bda9ebb86211a7147b
shiyapiku-dev <shiyapiku.dev@gmail.com>
2021-03-08 21:54:08 +0800

    Reduce bodyparser limit from 30mb to 15mb

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import cors from 'cors';
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({limit:"15mb", extended: true}));
-app.use(bodyParser.urlencoded({limit:"15mb", extended: true}));
+app.use(bodyParser.json({limit:"30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 app.use(cors());
 
 //Import Routes
